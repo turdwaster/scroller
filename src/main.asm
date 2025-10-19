47d547c2dbbc46ee770123a6681e2506273c3421
Per Lindén <per.linden@visionite.se>
2025-10-19 17:03:07 +0200

    Working level refill + extra interrupt, but now time issues for color move...

--- src/main.asm
+++ src/main.asm
@@ -2,15 +2,14 @@ scr0 = $0400
 scr1 = $2400
 colmem = $d800
 charsPerRow = 40
+levelWidth = 20
 lines = 24
 chunks = 4
 rowsPerChunk = 6
 chunkSize = charsPerRow * rowsPerChunk
 
-; Pointers
-LEVELDATA = $f8
-
 ; Bytes
+LEVELPOS = $fb
 SCROLLX = $fc
 SCROLLWORKPTR = $fd
 CHARBANK = $fe
@@ -28,10 +27,8 @@ main:
     lda #0
     sta SCROLLWORKPTR
     sta DELAY
-    lda #<level
-    sta LEVELDATA
-    lda #>level
-    sta LEVELDATA+1
+    lda #0
+    sta LEVELPOS
     jsr copyBacking
 
 install:
@@ -45,37 +42,67 @@ install:
     STA $DC0D           ; acknowledge pending interrupts from CIA-1
     STA $DD0D           ; acknowledge pending interrupts from CIA-2
 
-    LDA #170            ; set rasterline where interrupt shall occur (251 = start of lower border)
-    STA $D012           ; 53266
+    lda #$01            ; enable raster interrupt only
+    sta $d01a
 
+    LDA #249        ; set rasterline where interrupt shall occur (251 = start of lower border)
+    STA $D012           ; 53266
     lda #<irq           ; set IRQ vector low byte
     sta $0314
     lda #>irq           ; set IRQ vector high byte
     sta $0315
-    lda #$01            ; enable raster interrupt only
-    sta $d01a
     cli
     rts
+
+topIrq:
+    lda #$01
+    sta $d019
+    
+    ;lda $d018
+    ;and #15
+    ;ora #$10
+    ;sta $d018
+
+    lda $d016
+    and #255-15
+    sta $d016
+
+    lda #2
+    sta $d020
+    LDA #249
+    STA $D012
+    lda #<irq
+    sta $0314
+    lda #>irq
+    sta $0315
+    jmp $ea31
 
 irq:
     lda #$01
     sta $d019
-    lda #$01
+    lda #0
     sta $d020
 
-    inc DELAY
-    lda DELAY
-    cmp #20
-    bne exitirq
-    lda #0
-    sta DELAY
+    ;inc DELAY
+    ;lda DELAY
+    ;cmp #20
+    ;bne exitirq
+    ;lda #0
+    ;sta DELAY
 
     jsr skipandhop
 
 exitirq:
-    lda #$00
+    lda #0
     sta $d020
-    jmp $ea31 ; $EA81 = no other handling of IRQs
+
+    LDA #242
+    STA $D012
+    lda #<topIrq
+    sta $0314
+    lda #>irq
+    sta $0315
+    jmp $ea31
 
 skipandhop:
     dec SCROLLX         ; Move one pixel to the left
@@ -112,21 +139,21 @@ trampo:
     iny
     cpy #endWork-work
     bne stillWorkToDo
-    ldy #0
+    ldy #restart - work
 stillWorkToDo:
     sty SCROLLWORKPTR
     rts
 
-    !macro moveAChunk .scr,.c {
+    !macro moveAChunk .scr,.c,.step {
         .startChr = .scr + (.c - 1) * chunkSize
         ldx #0
 .chunkNext:
         !for r, rowsPerChunk {
-            lda .startChr + (r - 1) * charsPerRow + 2,x
+            lda .startChr + (r - 1) * charsPerRow + .step,x
             sta .startChr + (r - 1) * charsPerRow,x
         }
         inx
-        cpx #charsPerRow - 2
+        cpx #charsPerRow - .step
         beq .chunkDone
         jmp .chunkNext
 .chunkDone:
@@ -137,15 +164,44 @@ stillWorkToDo:
 
 moveChunk1:
     !for c, chunks {
-        +moveAChunk scr0, c
+        +moveAChunk scr0, c, 2
         !align chrMoveAlign-1, 0
     }
 
 moveChunk2:
     !for c, chunks {
-        +moveAChunk scr1, c
+        +moveAChunk scr1, c, 2
         !align chrMoveAlign-1, 0
     }
+
+copyBacking:
+    ; Set back buffer to front shifted one char left (ABCDEF -> BCDEF-)
+    ldx #charsPerRow-1
+backingNext:
+    !for r, lines  {
+        lda scr0 + (r - 1) * charsPerRow + 1,x
+        sta scr1 + (r - 1) * charsPerRow + 0,x
+    }
+    dex
+    bmi backingDone
+    jmp backingNext
+
+backingDone:
+    ; Clear bottom row
+    ldx #charsPerRow-1
+    lda #32
+clearMore:
+    sta scr1 + lines * charsPerRow,x
+    dex
+    bpl clearMore
+
+    ; Add first level column to back buffer (BCDEF- -> BCDEFX)
+    ldy #0
+    !for r, lines  {
+        lda level + (r - 1) * levelWidth,y
+        sta scr1 + (r - 1) * charsPerRow + (charsPerRow - 1)
+    }
+    rts
 
 moveColorsAndSwap:
     lda CHARBANK        ; Swap frame scr0/scr1
@@ -155,6 +211,14 @@ moveColorsAndSwap:
     and #15
     ora CHARBANK
     sta $d018
+
+moveColors:
+    ; Add new color column from level data
+    ldy LEVELPOS
+    !for r, lines  {
+        lda level + (r - 1) * levelWidth + 1,y
+        sta colmem + (r - 1) * charsPerRow + (charsPerRow - 1)
+    }
 
     ldx #0
 colorNext:
@@ -171,66 +235,60 @@ noWork:
     rts
 
 fillColumn1:
-    ldy #0
+    ldy LEVELPOS
+    .fillWidth = 2
     !for r, lines  {
-        lda (LEVELDATA),y
-        sta scr0 + (r - 1) * charsPerRow + (charsPerRow - 1)
-        iny
+        lda level + (r - 1) * levelWidth + 0,y
+        sta scr0 + (r - 1) * charsPerRow + (charsPerRow - .fillWidth) + 0
+    }
+    !for r, lines  {
+        lda level + (r - 1) * levelWidth + 1,y
+        sta scr0 + (r - 1) * charsPerRow + (charsPerRow - .fillWidth) + 1
     }
     rts
 
 fillColumn2:
-    ldy #0
+    ldy LEVELPOS
     !for r, lines  {
-        lda (LEVELDATA),y
-        sta scr1 + (r - 1) * charsPerRow + (charsPerRow - 1)
-        iny
+        lda level + (r - 1) * levelWidth + 0,y
+        sta scr1 + (r - 1) * charsPerRow + (charsPerRow - .fillWidth) + 0
+    }
+    !for r, lines  {
+        lda level + (r - 1) * levelWidth + 1,y
+        sta scr1 + (r - 1) * charsPerRow + (charsPerRow - .fillWidth) + 1
     }
     rts
 
 bumpLevelPtr:
-    ; Advance level data pointer
-    tya
-    clc
-    adc LEVELDATA
-    sta LEVELDATA
-    lda LEVELDATA+1
-    adc #0
-    sta LEVELDATA+1
+    ldy LEVELPOS
+    iny
+    ; Let it loop baby
+    ;cpy #levelWidth-1
+    ;bne stillHazLevel
+    ;ldy #0
+;stillHazLevel:
+    sty LEVELPOS
     rts
 
-; scr0      scr1
-; ==================
-;   ____
-; |ABCDEF|  | ABCDE|
-; 
-; moveChunk2
-;   ____
-; |ABCDEF|  |BCDE  |
-; 
-; fillColumn2
-; swap
-;             ____
-; |ABCDEF|  |BCDE X|
-; 
-; moveChunk1
-;             ____
-; |CDEF  |  |BCDE X|
-; 
-; fillColumn1
-;             ____
-; |CDEF X|  |ABCDEX|
+; Start = copy
+; |ABCDEF|  |ABCDEF|
+
+; Goal at first swap (LEVELPTR = Y)
+; |ABCDEF|  |BCDEFX|
+
+;init = moveChunk2single, fillChunk2single
 
 work:
-    !word moveChunk2
-    !word moveChunk2 + chrMoveAlign * 1
-    !word moveChunk2 + chrMoveAlign * 2
-    !word moveChunk2 + chrMoveAlign * 3
-    !word fillColumn2
+    !word noWork
+    !word noWork
+    !word noWork
+    !word noWork
+    !word noWork
     !word noWork
     !word noWork
     !word moveColorsAndSwap
 
+restart:
     !word moveChunk1
     !word moveChunk1 + chrMoveAlign * 1
     !word moveChunk1 + chrMoveAlign * 2
@@ -239,20 +297,16 @@ work:
     !word bumpLevelPtr
     !word noWork
     !word moveColorsAndSwap
-endWork:
 
-copyBacking:
-    ldx #charsPerRow-1
-backingNext:
-    !for r, lines  {
-        lda scr0 + (r - 1) * charsPerRow,x
-        sta scr1 + (r - 1) * charsPerRow + 1,x
-    }
-    dex
-    bmi backingDone
-    jmp backingNext
-backingDone:
-    rts
+    !word moveChunk2
+    !word moveChunk2 + chrMoveAlign * 1
+    !word moveChunk2 + chrMoveAlign * 2
+    !word moveChunk2 + chrMoveAlign * 3
+    !word fillColumn2
+    !word bumpLevelPtr
+    !word noWork
+    !word moveColorsAndSwap
+endWork:
 
 debugg:
     clc
@@ -268,8 +322,13 @@ okNum:
     sta colmem + 40 * lines + 2, x
     rts
 
+    !align 255, 0
 level:
+    !for r, lines  {
+        !byte 0, 1, 2, 3, 4, 5
+        !fill levelWidth - 6, 32
+    }
     !byte 1, 2, 3, 4, 5, 6
     !byte 1, 2, 3, 4, 5, 6
     !byte 1, 2, 3, 4, 5, 6
-    !byte 1, 2, 3, 4, 5, 6
+levelEnd:
