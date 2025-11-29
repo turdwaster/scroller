cb586bc1824c4f948c97c63b3ea5c2ca11ec02a0
Per Lindén <per.linden@visionite.se>
2025-11-29 21:07:51 +0100

    Clean up anim data

--- src/animdata.asm
+++ src/animdata.asm
@@ -4,20 +4,23 @@ rowStartHi:    	!for r, 0, CHARLINES-1 { !byte (r * CHARSPERROW) >> 8  }
 bitValuesX2:	!byte 1, 1, 2, 2, 4, 4, 8, 8, 16, 16, 32, 32, 64, 64, 128, 128
 
 ; Read-only anim structure, interleaved by ANIMSLOTS
-spawn_wait: 	!byte  0
-				!byte  5,  0,  0,  15,  5,  5,  5 ; Relative to last spawn!
+spawn_wait: 	!byte  0			; Player
+				!byte  5, 0, 0		; Traffic light
+				!byte  15, 5, 5, 5	; Loons
 				!align ANIMSLOTS-1, 0, 255
 anim_y:			!byte  50 | 128
-				!byte  0,  1,  2, 128 + 25, 128 + 50 , 128 + 75, 128 + 100, 255
+				!byte  0, 1, 2
+				!byte  128 + 25, 128 + 50 , 128 + 75, 128 + 100
 				!align ANIMSLOTS-1, 0, 0
 anim_stepdelay: !byte  0
-				!byte  25, 25, 25, 2, 4, 6, 8
+				!byte  25, 25, 25
+				!byte  2, 4, 6, 8
 				!align ANIMSLOTS-1, 0, 0
 anim_firstInstr:!byte  0
-				!byte  1,  6, 11,  springy,  springy,  springy,  springy
+				!byte  1, 6, 11
+				!byte  springy,  springy,  springy,  springy
 
 ; Instructions
-
 anim_instrs:	!byte 0
  				!byte 65,2, 1,    0,    256-4
 				!byte  1, 	65,2, 1,    256-4
