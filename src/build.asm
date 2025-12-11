f6ca8242269625ca6205f20e2f439107bb71e35c
Per Lindén <per.linden@visionite.se>
2025-12-11 10:12:37 +0100

    Switch build model to fix breakpoints/line information

---
+++ src/build.asm
@@ -0,0 +1,12 @@
+﻿!source "src/constants.asm"
+
+* = $0801
+!byte $0c,$08,$b5,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+
+!source "src/main.asm"
+!source "src/scroll.asm"
+!source "src/player.asm"
+!source "src/anim.asm"
+!source "src/animdata.asm"
+!source "src/leveldata.asm"
+!source "src/sprites.asm"
